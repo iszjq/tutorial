e5fcd2f950004ecd9dc6b01201d2a552c3b2adec
zjq <this@iszjq.cc>
2018-11-09 13:58:51 +0800

    员工Controller层

--- src/main/java/cc/iszjq/tutorial/controller/EmployeeController.java
+++ src/main/java/cc/iszjq/tutorial/controller/EmployeeController.java
@@ -1,8 +1,26 @@
 package cc.iszjq.tutorial.controller;
 
 
+import cc.iszjq.tutorial.dao.EmployeeDao;
+import cc.iszjq.tutorial.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    //员工列表页面
+    @GetMapping("/emps")
+    public String list(ModelMap map) {
+        Collection<Employee> employees = employeeDao.getAll();
+        map.put("emps", employees);
+        return "emp/list";
+    }
 }
--- src/main/java/cc/iszjq/tutorial/controller/HelloController.java
+++ src/main/java/cc/iszjq/tutorial/controller/HelloController.java
@@ -1,20 +1,13 @@
 package cc.iszjq.tutorial.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
-@Controller
+@RestController
 public class HelloController {
 
-
-
-    @GetMapping("/success")
-    public String success(ModelMap map) {
-        map.put("hello", "<h1>你好</h1>");
-        map.put("users", Arrays.asList("jiaqiang", "xiaonan"));
-        return "success";
+    @GetMapping("/error")
+    public String error() {
+        return "Something is error";
     }
 }
