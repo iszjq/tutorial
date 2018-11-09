1136192a18ffe193ceb8ecfe1b651e7dc9f4275e
zjq <this@iszjq.cc>
2018-11-09 09:12:55 +0800

    启用自己的MVC配置

--- src/main/java/cc/iszjq/tutorial/config/MyMvcConfig.java
+++ src/main/java/cc/iszjq/tutorial/config/MyMvcConfig.java
@@ -1,15 +1,43 @@
 package cc.iszjq.tutorial.config;
 
+import cc.iszjq.tutorial.component.LoginHandlerInterceptor;
+import cc.iszjq.tutorial.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
+    /**
+     * 设置路径映射
+     *
+     * @param registry
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");
         registry.addViewController("/index.html").setViewName("login");
+    }
+
+    //注册拦截器
+    //SpringBoot1.x已经做好了静态资源映射
+    //SpringBoot2.x发生了变化，需要自己处理
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/index.html", "/", "/user/login", "/asserts/**");
+    }
+
+    /**
+     * 调用自己的区域解析器
+     *
+     * @return
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
     }
 }
