35588fff75ebf8bf0866cd3b3d1ffcde5047734c
zjq <this@iszjq.cc>
2018-11-13 10:14:37 +0800

    自定义错误状态信息

---
+++ src/main/java/cc/iszjq/tutorial/component/MyErrorAttributes.java
@@ -0,0 +1,20 @@
+package cc.iszjq.tutorial.component;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Component
+public class MyErrorAttributes extends DefaultErrorAttributes {
+
+    //返回值的Map就是页面能获取到的值
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        Map<String, Object> map = super.getErrorAttributes(webRequest, includeStackTrace);
+        map.put("ext", webRequest.getAttribute("ext", 0));
+        return map;
+
+    }
+}
