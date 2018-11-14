0f715218646494ba052c543d910365c6542ec161
zjq <this@iszjq.cc>
2018-11-14 14:30:02 +0800

    定制监听器

---
+++ src/main/java/cc/iszjq/tutorial/listener/MyListener.java
@@ -0,0 +1,16 @@
+package cc.iszjq.tutorial.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println(".........web应用启动..........");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println(".........web应用销毁..........");
+    }
+}
